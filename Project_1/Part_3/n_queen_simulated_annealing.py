e76e310340b654117f6ca053e3287456b1613c9e
Eirik Folkestad <eirik.ek.folkestad@gmail.com>
2016-09-08 13:27:41 +0200

    test

--- Project_1/Part_3/n_queen_simulated_annealing.py
+++ Project_1/Part_3/n_queen_simulated_annealing.py
@@ -22,6 +22,7 @@ def simulated_annealing(init_state, init_temp, temp_stop):
             temp = temp_decay(temp)
         state = get_state(state)
         temp = init_temp
+        
 
 def get_state(state):
     return random_state(state)
